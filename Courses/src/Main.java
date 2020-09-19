ed34e8cb6fa48eab5e3cc848bd2fa036d719d319
Fomina Aleksandra <sashavera1998@gmail.com>
2020-09-19 12:53:16 +0500

    class Mentor - changed Constructor, class Main - changed createTasks

--- Courses/src/Main.java
+++ Courses/src/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Task[] tasks = new Task[100];
+        Random random = new Random();
         createTasks(tasks);
-        Mentor mentor1 = new Mentor("Иван", "Иванов", 25);
-        Mentor mentor2 = new Mentor("Марина", "Петрова", 25);
+        Mentor mentor1 = new Mentor("Иван", "Иванов", 25, random);
+        Mentor mentor2 = new Mentor("Марина", "Петрова", 25, random);
         Student[] students = new Student[10];
         createStudents(students, mentor1, mentor2);
-        Random random = new Random();
         solveTasksByStudents(students, tasks, random);
 
     }
 
     private static void solveTasksByStudents(Student[] students, Task[] tasks, Random random) {
         Scanner scanner = new Scanner(System.in);
-        while (Student.getCountSolvedTaskForAll() != tasks.length * students.length) {
+        while (Student.getCountSolvedTaskForAll() < tasks.length * students.length) {
             for (Student student : students) {
                 while (!student.isAllTasksSolved()) {
                     int count = random.nextInt(tasks.length);
@@ -28,23 +28,14 @@ public class Main {
 
     private static void createTasks(Task[] tasks) {
         for (int i = 0; i < tasks.length; ++i) {
+            Task task = null;
             switch (i % 3) {
-                case 0 -> {
-                    Test test = new Test();
-                    test.setNumber(i + 1);
-                    tasks[i] = test;
-                }
-                case 1 -> {
-                    Code code = new Code();
-                    code.setNumber(i + 1);
-                    tasks[i] = code;
-                }
-                case 2 -> {
-                    DragAndDrop dAndD = new DragAndDrop();
-                    dAndD.setNumber(i + 1);
-                    tasks[i] = dAndD;
-                }
+                case 0 -> task = new Test();
+                case 1 -> task = new Code();
+                case 2 -> task = new DragAndDrop();
             }
+            task.setNumber(i+1);
+            tasks[i]=task;
         }
     }
 
